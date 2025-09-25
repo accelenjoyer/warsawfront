355db827275131e6cdc7c2ab637052113f43f88d
accelenjoyer <doroshkirill1@mail.ru>
2025-09-25 16:32:30 +0300

    img fix

--- next.config.mjs
+++ next.config.mjs
@@ -2,7 +2,7 @@
 const nextConfig = {
     reactStrictMode: true,
     images: {
-        domains: ['www.the-warsaw.pl','warszawa-diaspora.pl'],
+        domains: ['www.the-warsaw.pl','warszawa-diaspora.pl','warsawhere.com'],
     },
 };
 
